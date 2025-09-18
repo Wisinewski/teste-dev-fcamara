113faee12e4df434bb696b052c28ccae47b72b66
Giancarlo Wisinewski Gomes <44417337+Wisinewski@users.noreply.github.com>
2025-09-18 17:51:00 -0300

    Create Downloader.cs

---
+++ c#/Downloader.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+public class Downloader
+{
+    private static List<string> cache = new List<string>();
+
+    public static async Task Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            DownloadAsync("https://example.com/data/" + i);
+        }
+
+        Console.WriteLine("Downloads started");
+        Console.WriteLine("Cache size: " + cache.Count);
+    }
+
+    private static async Task DownloadAsync(string url)
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            var data = await client.GetStringAsync(url);
+            cache.Add(data);
+        }
+    }
+}
